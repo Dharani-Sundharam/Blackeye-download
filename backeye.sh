b3b4c436b65e6c2afabed6fd7fb11899bcd26577
Dharani YT <79736125+DharaniYT@users.noreply.github.com>
2021-02-27 10:38:39 +0530

    Add files via upload

---
+++ backeye.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo
+echo ; sudo apt install figlet ;
+echo
+echo ; sudo apt install lolcat ;
+echo
+echo ; clear ;
+echo 
+echo ; figlet Dharani YT | lolcat ;
+echo
+echo "Scubscribe to my channel ;) " 
+echo 
+echo ; figlet Subscribe | lolcat ;
+echo 
+echo "Installing Blackeye [*]"; git clone https://github.com/x3rz/blackeye.git ;
+echo
+echo  ; sleep 5 ;
+echo ; clear ;
+echo
+echo ; cd blackeye ;
+echo 
+echo ; bash blackeye.sh ;
+echo
+
+ 
+  
